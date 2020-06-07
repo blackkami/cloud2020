8be242e6a9087b73b5c4b6ca2bff41ceaf85f055
kami <kamiitmonkey@163.com>
2020-06-07 18:35:56 +0800

    p56 consumerHystrix80全局降级方法

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/controller/OrderHystrixController.java
@@ -1,6 +1,7 @@
 package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
@@ -9,10 +10,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.concurrent.TimeUnit;
 
 @RestController
 @Slf4j
+@DefaultProperties(defaultFallback = "globalFallback")
 public class OrderHystrixController {
 
     @Resource
@@ -26,9 +27,10 @@ public class OrderHystrixController {
     }
 
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
-    @HystrixCommand(fallbackMethod = "paymentInfo_TimeoutHandler",commandProperties = {
-            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="2000")
-    })
+//    @HystrixCommand(fallbackMethod = "paymentInfo_TimeoutHandler",commandProperties = {
+//            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="2000")
+//    })
+    @HystrixCommand
     public String paymentInfo_Timeout(@PathVariable("id") Integer id){
         int age = 10/0;
         String result = paymentHystrixService.paymentInfo_Timeout(id);
@@ -37,6 +39,11 @@ public class OrderHystrixController {
 
     public String paymentInfo_TimeoutHandler(Integer id) {
         return "我是消费者80，对方支付系统繁忙，请10秒钟后再试或自己出错";
+    }
+
+
+    public String globalFallback() {
+        return "Global全局降级方法";
     }
 
 
