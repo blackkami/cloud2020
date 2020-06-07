175d0070d6b43515c24d824fcf9af25d6f2a6c4c
kami <kamiitmonkey@163.com>
2020-06-07 17:57:12 +0800

    p54 paymentHystrix8001降级

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/PaymentHystrixMain8001.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/PaymentHystrixMain8001.java
@@ -3,10 +3,12 @@ package com.atguigu.springcloud;
 import com.sun.xml.internal.ws.message.source.PayloadSourceMessage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class PaymentHystrixMain8001 {
 
     public static void main(String[] args) {
--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/impl/PaymentServiceImpl.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/impl/PaymentServiceImpl.java
@@ -1,6 +1,8 @@
 package com.atguigu.springcloud.service.impl;
 
 import com.atguigu.springcloud.service.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -12,13 +14,24 @@ public class PaymentServiceImpl implements PaymentService {
         return "线程池： " + Thread.currentThread().getName() + "paymentInfo_OK,id: " + id;
     }
 
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeoutHandler",commandProperties = {
+            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="5000")
+    })
     public String paymentInfo_Timeout(Integer id) {
         int timeNumber = 3;
+//        int age = 10/0;
         try {
             TimeUnit.SECONDS.sleep(timeNumber);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        return "线程池： " + Thread.currentThread().getName() + "paymentInfo_Timeout,id: " + id + "   耗时(秒)：" + timeNumber;
+        return "线程池： " + Thread.currentThread().getName() + " paymentInfo_Timeout，id: " + id + "\t耗时(秒)：" + timeNumber;
     }
+
+    public String paymentInfo_TimeoutHandler(Integer id) {
+        return "线程池： " + Thread.currentThread().getName() + " 系统繁忙或者运行报错，请稍后再试，id: " + id + "\t o(╥﹏╥)o：";
+    }
+
+
+
 }
