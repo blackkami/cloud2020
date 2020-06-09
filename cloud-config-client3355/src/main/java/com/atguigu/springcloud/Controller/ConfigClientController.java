2612dd1aad70a5b369f7b960b85cb7b87cf1e954
kami <kamiitmonkey@163.com>
2020-06-09 11:42:24 +0800

    p77 Config 伪动态获取最新配置

--- cloud-config-client3355/src/main/java/com/atguigu/springcloud/Controller/ConfigClientController.java
+++ cloud-config-client3355/src/main/java/com/atguigu/springcloud/Controller/ConfigClientController.java
@@ -1,10 +1,12 @@
 package com.atguigu.springcloud.Controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConfigClientController {
 
     @Value("${config.info}")    //这个config.info是在拉取的配置yml里写的
@@ -13,6 +15,7 @@ public class ConfigClientController {
 
     @GetMapping("/configInfo")
     public String getConfigInfo(){
+        //当远程（Git）修改配置文件后，还需要触发POST请求 /actuator/refresh
         return configInfo;
     }
 
