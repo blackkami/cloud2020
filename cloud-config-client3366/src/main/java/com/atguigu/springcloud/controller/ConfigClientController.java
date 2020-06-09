1c97210c6945f1e25a354a250fadb1423c9e4775
kami <kamiitmonkey@163.com>
2020-06-09 16:27:09 +0800

    p82 Bus通知Config Server达到广播刷新配置目的

---
+++ cloud-config-client3366/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,25 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")    //这个config.info是在拉取的配置yml里写的
+    private String configInfo;
+
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return "serverPort: "+serverPort+"\t configInfo: "+configInfo;
+    }
+
+
+}
