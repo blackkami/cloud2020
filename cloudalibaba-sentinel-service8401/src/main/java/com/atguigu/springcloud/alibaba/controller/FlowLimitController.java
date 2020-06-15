66112ad2c3b3ca0dcce94ff89f645a6d312893ea
kami <kamiitmonkey@163.com>
2020-06-15 11:04:24 +0800

    p123 降级规则 8401

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,11 +1,13 @@
 package com.atguigu.springcloud.alibaba.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
 
 @RestController
+@Slf4j
 public class FlowLimitController {
 
     @GetMapping("/testA")
@@ -21,6 +23,28 @@ public class FlowLimitController {
     @GetMapping("/testB")
     public String testB(){
         return "---------testB";
+    }
+
+
+    @GetMapping("/testD")
+    public String testD(){
+//        try {
+//            TimeUnit.SECONDS.sleep(1);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        log.info("testD 测试RT");
+        int age = 10/0;
+        log.info("testD 测试异常比例");
+        return "---------testD";
+    }
+
+
+    @GetMapping("/testE")
+    public String testE(){
+        int age = 10/0;
+        log.info("testE 测试异常数");
+        return "---------testE";
     }
 
 }
